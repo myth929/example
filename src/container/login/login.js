a7376451c30073adcdc5d9bf6d979b76215ed630
myth929 <422896780@qq.com>
2018-07-16 16:58:14 +0800

    Delecte node_modules

--- src/container/login/login.js
+++ src/container/login/login.js
@@ -5,17 +5,16 @@ import { connect } from 'react-redux'
 import { Redirect } from 'react-router-dom'
 import {login} from '../../redux/user.redux'
 
+import imoocForm from '../../component/form/fifa-form'
+
 @connect(
     state=>state.user,
     {login}
 )
+@imoocForm
 class Login extends React.Component{
     constructor(props){
         super(props)
-        this.state={
-            user:'',
-            pwd:''
-        }
         this.register = this.register.bind(this)
         this.handleLogin = this.handleLogin.bind(this)
     }
@@ -24,13 +23,8 @@ class Login extends React.Component{
         this.props.history.push('/register')
     }
 
-    handleChange(key,val){
-        this.setState({
-            [key]:val
-        })
-    }
     handleLogin(){
-        this.props.login(this.state)
+        this.props.login(this.props.state)
     }
 
     render(){
@@ -42,12 +36,12 @@ class Login extends React.Component{
                     <List>
                         {this.props.msg?<p className='error-msg'>{this.props.msg}</p>:null}
                         <InputItem
-                            onChange={v=>this.handleChange('user',v)}
+                            onChange={v=>this.props.handleChange('user',v)}
                         >用户</InputItem>
                         <WhiteSpace />
                         <InputItem
                             type='password'
-                            onChange={v=>this.handleChange('pwd',v)}
+                            onChange={v=>this.props.handleChange('pwd',v)}
                         >密码</InputItem>
                     </List>
                     <Button onClick={this.handleLogin} type='primary'>登录</Button>
--- src/container/register/register.js
+++ src/container/register/register.js
@@ -5,33 +5,26 @@ import { connect } from 'react-redux'
 import { Redirect } from 'react-router-dom'
 import { register } from '../../redux/user.redux'
 import '../../index.css'
+import imoocForm from '../../component/form/fifa-form'
 
 @connect(
     state=>state.user,
     {register}
 )
-
+@imoocForm
 
 class Register extends React.Component{
     constructor(props){
         super(props);
-        this.state = {
-            user:'',
-            pwd:'',
-            repeatpwd:'',
-            type:'genius' //或者boss
-        }
 
         this.handleRegister = this.handleRegister.bind(this)
     }
-    handleChange(key,val){
-        this.setState({
-            [key]:val
-        })
+    componentDidMount(){
+        this.props.handleChange('type','genius')
     }
 
     handleRegister(){
-        this.props.register(this.state)
+        this.props.register(this.props.state)
     }
 
     render(){
@@ -43,28 +36,28 @@ class Register extends React.Component{
                 <List>
                     {this.props.msg?<p className='error-msg'>{this.props.msg}</p>:null}
                     <InputItem
-                        onChange={v=>this.handleChange('user',v)}
+                        onChange={v=>this.props.handleChange('user',v)}
                     >用户</InputItem>
                     <WhiteSpace />
                     <InputItem
                         type='password'
-                        onChange={v=>this.handleChange('pwd',v)}
+                        onChange={v=>this.props.handleChange('pwd',v)}
                     >密码</InputItem>
                     <WhiteSpace />
                     <InputItem
                         type='password'
-                        onChange={v=>this.handleChange('repeatpwd',v)}
+                        onChange={v=>this.props.handleChange('repeatpwd',v)}
                     >确认密码</InputItem>
                     <WhiteSpace />
                     <RadioItem 
-                        checked={this.state.type === 'genius'}
-                        onChange={() => this.handleChange('type', 'genius')}
+                        checked={this.props.state.type === 'genius'}
+                        onChange={() => this.props.handleChange('type', 'genius')}
                     >
                         萌豚
                     </RadioItem>
                     <RadioItem 
-                        checked={this.state.type === 'boss'}
-                        onChange={() => this.handleChange('type', 'boss')}
+                        checked={this.props.state.type === 'boss'}
+                        onChange={() => this.props.handleChange('type', 'boss')}
                     >
                         废宅
                     </RadioItem>

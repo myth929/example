d633fe68f6697004a16b6205601f2aad587b5400
myth929 <422896780@qq.com>
2018-07-17 17:15:14 +0800

    实时通信初步实现
    技术基础：socket.io

--- server/user.js
+++ server/user.js
@@ -4,13 +4,31 @@ const utils = require('utility')
 const Router = express.Router()
 const model = require('./model')
 const User = model.getModel('user')
+const Chat = model.getModel('chat')
 const _filter = {'pwd':0,'__v':0}
+
+Chat.remove({},function (e,d) {})             //清空聊天信息
 
 Router.get('/list',function (req,res) {
     const {type} = req.query
     // User.remove({},function (e,d) {})
     User.find({type},function (err,doc) {
         return res.json({code:0,data:doc})
+    })
+})
+Router.get('/getmsgList',function (req,res) {
+    const user = req.cookies.userid
+    
+    User.find({},function (e,userdoc) {
+        let users = {}
+        userdoc.forEach(v=>{
+            users[v._id] = {name:v.user, avatar:v.avatar}
+        })
+        Chat.find({'$or':[{from:user},{to:user}]},function (err,doc) {
+            if(!err){
+                return res.json({code:0,msgs:doc,users:users})
+            }
+        })
     })
 })
 Router.post('/update',function (req,res) {
---
+++ src/component/chat/chat.js
@@ -0,0 +1,90 @@
+
+import React from 'react'
+import { List,InputItem,NavBar,Icon } from 'antd-mobile'
+import io from 'socket.io-client'
+import { connect } from "react-redux";
+import { getMsgList, sendMsg, recvMsg } from "../../redux/chat.redux";
+
+const socket = io('ws://localhost:9093')
+
+@connect(
+    state=>state,
+    { getMsgList, sendMsg, recvMsg }
+)
+class Chat extends React.Component{
+    constructor(props){
+        super(props)
+        this.state = {text:'',msg:[]}
+    }
+    componentDidMount(){
+        if(!this.props.chat.chatmsg.length){
+            this.props.getMsgList()
+            this.props.recvMsg()
+        } 
+    }
+    handleSubmit(){
+        // socket.emit('sendmsg',{text:this.state.text})
+        // this.setState({text:''})
+        const from = this.props.user._id
+        const to = this.props.match.params.user
+        const msg = this.state.text
+        this.props.sendMsg({from,to,msg})
+        this.setState({text:''})
+
+    }
+    render(){
+        const userid = this.props.match.params.user
+        const Item = List.Item
+        const users = this.props.chat.users
+        if(!users[userid]){
+            return null
+        }
+        return(
+            <div id='chat-page'>
+                <NavBar 
+                    mode='dark'
+                    icon={<Icon type="left" />}
+                    onLeftClick={()=>{
+                        this.props.history.goBack()
+                    }}
+                >
+                    {users[userid].name}
+                </NavBar>
+
+                {this.props.chat.chatmsg.map(v=>{
+                    const avatar = require(`../img/${users[v.from].avatar}.jpg`)
+                    return v.from == userid?(
+                        <List key={v._id}>
+                            <Item
+                                thumb={avatar}
+                            >{v.content}</Item>
+                        </List>
+                    ):(
+                        <List key={v._id}>
+                            <Item 
+                                extra={<img src={avatar} />}
+                                className='chat-me'
+                            >{v.content}</Item>
+                        </List>
+                    )
+                })}
+                <div className="stick-footer">
+                    <List>
+                        <InputItem
+                            placeholder='请输入'
+                            value={this.state.text}
+                            onChange={v => {
+                                this.setState({ text: v })
+                            }}
+                            extra={<span onClick={() => this.handleSubmit()}>发送</span>}
+                        ></InputItem>
+                    </List>
+                </div>
+            </div>
+            
+            
+        )
+    }
+}
+
+export default Chat
